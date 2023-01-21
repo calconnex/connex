9a14dd50af941518ff9b93f2b8ccdffdc7e86333
James Chong <jameschong@Jamess-MacBook-Pro.local>
2023-01-21 14:23:57 -0800

    compare page added

--- divjoy-template/src/original/js/Compare.js
+++ divjoy-template/src/original/js/Compare.js
@@ -2,16 +2,18 @@ import ApplicantCompare from "./ApplicantCompare";
 // import useFetch from "./useFetch";
 import data from "../data/db.json";
 import Navbar from "./Navbar";
+import { useAllApplicants } from "../../util/db";
 
 const Compare = () => {
   // const { error, isPending, data: applicants } = useFetch('http://localhost:8000/applicants')
+  const newData = useAllApplicants();
 
   return (
     <div className="compare">
       <Navbar/>
       {/* { error && <div>{ error }</div> }
       { isPending && <div>Loading...</div> } */}
-      { data.applicants && <ApplicantCompare applicants={data.applicants} /> }
+      { data.applicants && <ApplicantCompare applicants={newData.data} /> }
     </div>
   );
 }
