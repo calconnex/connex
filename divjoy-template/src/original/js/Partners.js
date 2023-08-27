ce18217e010f0b85580720d4ec9f75872ab1245b
Rushil Jerfy <rushiljerfy@berkeley.edu>
2023-08-27 02:01:08 -0700

    added logos to partners page

--- divjoy-template/src/original/js/Partners.js
+++ divjoy-template/src/original/js/Partners.js
@@ -5,15 +5,25 @@ import home from "../icons/home.png"
 import compare from "../icons/compare.png"
 import { useAuth } from "../../util/auth";
 import valley from "../icons/valley_logo.png";
+import voyager from "../icons/voyager.png";
+import launchpad from "../icons/launchpad.png";
+import blockchain from "../icons/blockchain.png";
+import webdev_logo from "../icons/webdev_logo.png";
+import cmg_logo from "../icons/cmg_logo.png";
+import blueprint_logo from "../icons/blueprint_logo.png";
+import netimpact from "../icons/netimpact.png";
+import bbs_logo from "../icons/bbs_logo.png";
+import mlab_logo from "../icons/mlab_logo.png";
+import codeology_logo from "../icons/codeology_logo.png";
 import full from "../icons/full_logo.png";
 import plain from "../icons/plainwhite.png";
 
 import styles from '../css/partners.module.css';
 
-const GeneralClickablePartner = ({clubName, categoryOne, categoryTwo, numMembers}) => (
+const GeneralClickablePartner = ({logo, clubName, categoryOne, categoryTwo, numMembers}) => (
   <div className={styles['ClubFrame']}>
           <img
-            src = {valley}
+            src = {logo}
             alt="Net Impact Logo"
             className={styles['ClubLogo']}
           />
@@ -70,17 +80,17 @@ const CoffeeChatsApplicant6 = (props) => {
         </span>
 
       <div style={gridStyle}>
-        <GeneralClickablePartner clubName="Valley Consulting" categoryOne="Technology" categoryTwo="Consulting" numMembers={30}/>
-        <GeneralClickablePartner clubName="CMG Strategy Consulting" categoryOne="FILL IN INFO" categoryTwo="Consulting" numMembers={30}/>
-        <GeneralClickablePartner clubName="Codeology" categoryOne="Technology" categoryTwo="FILL IN INFO" numMembers={30}/>
-        <GeneralClickablePartner clubName="Launchpad" categoryOne="Technology" categoryTwo="FILL IN INFO" numMembers={40}/>
-        <GeneralClickablePartner clubName="Blockchain at Berkeley" categoryOne="Technology" categoryTwo="Consulting" numMembers={30}/>
-        <GeneralClickablePartner clubName="Web Development at Berkeley" categoryOne="Technology" categoryTwo="Consulting" numMembers={55}/>
-        <GeneralClickablePartner clubName="Blueprint, Technology for Nonprofits" categoryOne="Technology" categoryTwo="Consulting" numMembers={30}/>
-        <GeneralClickablePartner clubName="Voyager Consulting" categoryOne="FILL IN INFO" categoryTwo="Consulting" numMembers={40}/>
-        <GeneralClickablePartner clubName="Berkeley Business Society" categoryOne="Business" categoryTwo="Consulting" numMembers={45}/>
-        <GeneralClickablePartner clubName="Net Impact" categoryOne="Social Impact" categoryTwo="Consulting" numMembers={35}/>
-        <GeneralClickablePartner clubName="Machine Learning at Berkeley" categoryOne="Technology" categoryTwo="FILL IN INFO" numMembers={60}/>
+        <GeneralClickablePartner logo={valley}clubName="Valley Consulting" categoryOne="Technology" categoryTwo="Consulting" numMembers={30}/>
+        <GeneralClickablePartner logo={launchpad} clubName="Launchpad" categoryOne="Technology" categoryTwo="FILL IN INFO" numMembers={40}/>
+        <GeneralClickablePartner logo={voyager} clubName="Voyager Consulting" categoryOne="FILL IN INFO" categoryTwo="Consulting" numMembers={40}/>
+        <GeneralClickablePartner logo={blockchain} clubName="Blockchain at Berkeley" categoryOne="Technology" categoryTwo="Consulting" numMembers={30}/>
+        <GeneralClickablePartner logo={webdev_logo} clubName="Web Development at Berkeley" categoryOne="Technology" categoryTwo="Consulting" numMembers={55}/>
+        <GeneralClickablePartner logo={cmg_logo} clubName="CMG Strategy Consulting" categoryOne="FILL IN INFO" categoryTwo="Consulting" numMembers={30}/>
+        <GeneralClickablePartner logo={blueprint_logo} clubName="Blueprint, Technology for Nonprofits" categoryOne="Technology" categoryTwo="Consulting" numMembers={30}/>
+        <GeneralClickablePartner logo={netimpact} clubName="Net Impact" categoryOne="Social Impact" categoryTwo="Consulting" numMembers={35}/>
+        <GeneralClickablePartner logo={bbs_logo} clubName="Berkeley Business Society" categoryOne="Business" categoryTwo="Consulting" numMembers={45}/>
+        <GeneralClickablePartner logo={mlab_logo}clubName="Machine Learning at Berkeley" categoryOne="Technology" categoryTwo="FILL IN INFO" numMembers={60}/>
+        <GeneralClickablePartner logo={codeology_logo} clubName="Codeology" categoryOne="Technology" categoryTwo="FILL IN INFO" numMembers={30}/>
       </div>
 
 
